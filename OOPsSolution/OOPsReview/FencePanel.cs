ae6a0abfa6a9b28b8c43f7a063008f4b84db9314
Mathieu Hébert <mhbert1@nait.ca>
2019-09-09 12:08:01 -0600

    ooosSolution

---
+++ OOPsSolution/OOPsReview/FencePanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    //by default all class are private
+    //for this class tobe used by an outside user
+    //   you must alter the permission for this class
+    //   you will use public
+
+    public class FencePanel
+    {
+    }
+}
